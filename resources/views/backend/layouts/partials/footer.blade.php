d616fd011bbab30396fd1f5f36df46d3875f3ca6
Masud Rana Tapu <masudranatapu@gmail.com>
2024-01-11 14:06:20 +0600

    done

---
+++ resources/views/backend/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="footer-left">
+    <a href="javascript:;">Templateshub</a>
+</div>
+<div class="footer-right">
+    <a href="javascript:;">Templateshub</a>
+</div>
---
+++ resources/views/backend/layouts/partials/style.blade.php
@@ -0,0 +1,5 @@
+<!-- General CSS Files -->
+<link rel="stylesheet" href="{{ asset('backend/css/app.css') }}">
+<!-- Template CSS -->
+<link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('backend/css/components.css') }}">
