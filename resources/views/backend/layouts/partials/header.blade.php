164b60e617c96f5b509d368335d86808e445bab3
Masud Rana Tapu <masudranatapu@gmail.com>
2024-01-15 14:05:22 +0600

    doen

--- resources/views/backend/layouts/partials/header.blade.php
+++ resources/views/backend/layouts/partials/header.blade.php
@@ -1,11 +1,15 @@
 <div class="form-inline mr-auto">
     <ul class="navbar-nav mr-3">
-        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg
-                    collapse-btn">
-                <i data-feather="align-justify"></i></a></li>
-        <li><a href="#" class="nav-link nav-link-lg fullscreen-btn">
+        <li>
+            <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg collapse-btn">
+                <i data-feather="align-justify"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link nav-link-lg fullscreen-btn">
                 <i data-feather="maximize"></i>
-            </a></li>
+            </a>
+        </li>
         <li>
             <form class="form-inline mr-auto">
                 <div class="search-element">
@@ -20,15 +24,20 @@
     </ul>
 </div>
 <ul class="navbar-nav navbar-right">
-    <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
-            class="nav-link nav-link-lg message-toggle"><i data-feather="mail"></i>
+    <li class="dropdown dropdown-list-toggle">
+        <a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle">
+            <i data-feather="mail"></i>
             <span class="badge headerBadge1">
-                6 </span> </a>
+                6
+            </span>
+        </a>
         <div class="dropdown-menu dropdown-list dropdown-menu-right pullDown">
             <div class="dropdown-header">
                 Messages
                 <div class="float-right">
-                    <a href="#">Mark All As Read</a>
+                    <a href="#">
+                        Mark All As Read
+                    </a>
                 </div>
             </div>
             <div class="dropdown-list-content dropdown-list-message">
@@ -79,12 +88,16 @@
                 </a>
             </div>
             <div class="dropdown-footer text-center">
-                <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                <a href="#">
+                    View All
+                    <i class="fas fa-chevron-right"></i>
+                </a>
             </div>
         </div>
     </li>
-    <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
-            class="nav-link notification-toggle nav-link-lg"><i data-feather="bell" class="bell"></i>
+    <li class="dropdown dropdown-list-toggle">
+        <a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg">
+            <i data-feather="bell" class="bell"></i>
         </a>
         <div class="dropdown-menu dropdown-list dropdown-menu-right pullDown">
             <div class="dropdown-header">
@@ -131,28 +144,38 @@
                 </a>
             </div>
             <div class="dropdown-footer text-center">
-                <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                <a href="#">
+                    View All
+                    <i class="fas fa-chevron-right"></i>
+                </a>
             </div>
         </div>
     </li>
-    <li class="dropdown"><a href="#" data-toggle="dropdown"
-            class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image"
-                src="{{ asset('backend/img/user.png') }}" class="user-img-radious-style"> <span
-                class="d-sm-none d-lg-inline-block"></span></a>
+    <li class="dropdown">
+        <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            <img alt="image" src="{{ asset('backend/img/user.png') }}" class="user-img-radious-style">
+            <span class="d-sm-none d-lg-inline-block"></span>
+        </a>
         <div class="dropdown-menu dropdown-menu-right pullDown">
-            <div class="dropdown-title">Hello Sarah Smith</div>
-            <a href="profile.html" class="dropdown-item has-icon"> <i
-                    class="far
-                        fa-user"></i> Profile
-            </a> <a href="timeline.html" class="dropdown-item has-icon"> <i class="fas fa-bolt"></i>
+            <div class="dropdown-title">Hello {{ Auth::user()->name }}</div>
+            <a href="profile.html" class="dropdown-item has-icon">
+                <i class="far fa-user"></i>
+                Profile
+            </a>
+            <a href="timeline.html" class="dropdown-item has-icon"> <i class="fas fa-bolt"></i>
                 Activities
-            </a> <a href="#" class="dropdown-item has-icon"> <i class="fas fa-cog"></i>
+            </a>
+            <a href="#" class="dropdown-item has-icon"> <i class="fas fa-cog"></i>
                 Settings
             </a>
             <div class="dropdown-divider"></div>
-            <a href="auth-login.html" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
-                Logout
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item has-icon text-danger">
+                <i class="fas fa-sign-out-alt"></i>
+                {{ __('Logout') }}
             </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
         </div>
     </li>
 </ul>
