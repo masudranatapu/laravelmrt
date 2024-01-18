1d4489700a3e0457c6a91c078a0f6113ac407c93
Masud Rana Tapu <masudranatapu@gmail.com>
2024-01-18 18:22:44 +0600

    done

--- resources/views/backend/layouts/partials/script.blade.php
+++ resources/views/backend/layouts/partials/script.blade.php
@@ -2,7 +2,7 @@
 <script src="{{ asset('backend/js/app.min.js') }}"></script>
 <!-- JS Libraies -->
 <!-- Page Specific JS File -->
-<script src="{{ asset('backend/js/page/index.js') }}"></script>
+{{-- <script src="{{ asset('backend/js/page/index.js') }}"></script> --}}
 <!-- Template JS File -->
 <script src="{{ asset('backend/js/scripts.js') }}"></script>
 <!-- Custom JS File -->
